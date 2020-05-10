4e0ae9aeb6b1121c6015ed259bd26d57455d6345
vijayraavi <vijay.r57@outlook.com>
2020-05-10 10:04:06 +0000

    Updated Startup text...

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace AspNetCore_Demo
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Azure DevOps!!!!");
                 });
             });
         }
